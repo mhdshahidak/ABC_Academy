c18f16fd3cf9518b99f96a2cb575eac638b1d142
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-05-16 09:14:05 +0530

    website:home,abou,courses

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,14 @@
 from . import views
 from django.urls import path
 
-app_name ='adminapp'
+app_name ='web'
 
 urlpatterns = [
-    
+    path('',views.homepage,name='home'),
+    path('allcourse',views.allcourses,name='allcourse'),
+    path('ttccourse',views.course_ttc,name='ttccourse'),
+    path('fashioncourse',views.course_fashion,name='fashioncourse'),
+    path('onlineapplication',views.online_application,name='onlineapplication'),
+    path('about',views.about_us,name='about'),
+    path('tour',views.tourtocollege,name='tours'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    return render(request,'website/home.html')
+
+def allcourses(request):
+    return render(request,'website/all_courses.html')
+
+def course_ttc(request):
+    return render(request,'website/ttc.html')
+
+def course_fashion(request):
+    return render(request,'website/fashion.html')
+
+def online_application(request):
+     return render(request,'website/apply_online.html')
+
+def about_us(request):
+     return render(request,'website/about_us.html')
+
+def tourtocollege(request):
+     return render(request,'website/tour.html')
+
