eda20e7f48bc9dafe377d4e1e2ef31abfe085c9e
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-05-26 15:40:40 +0530

    teacher change

--- teacher/views.py
+++ teacher/views.py
@@ -3,23 +3,44 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request,'teacher/teacher_home.html')
+    context = {
+        "is_homepage": True,
+        }
+    return render(request,'teacher/teacher_home.html',context)
 
 def profile(request):
-    return render(request,'teacher/profile.html')
+    context = {
+        "is_profile": True,
+        }
+    return render(request,'teacher/profile.html',context)
 
 def edit_profile(request):
-    return render(request,'teacher/edit_teacher.html')
+    context = {
+        "is_editprofile": True,
+        }
+    return render(request,'teacher/edit_teacher.html',context)
 
 def courses(request):
-    return render(request,'teacher/courses.html')
+    context = {
+        "is_courses": True,
+        }
+    return render(request,'teacher/courses.html',context)
 
 def studentlist(request):
-    return render(request,'teacher/studentlist.html')
+    context = {
+        "is_studentlist": True,
+        }
+    return render(request,'teacher/studentlist.html',context)
 
 def coursewise_studentlist(request):
-    return render(request,'teacher/coursewise_studentlist.html')
+    context = {
+        "is_coursewisestudentlist": True,
+        }
+    return render(request,'teacher/coursewise_studentlist.html',context)
 
 def calander(request):
-    return render(request,'teacher/calender.html')
+    context = {
+        "is_calander": True,
+        }
+    return render(request,'teacher/calender.html',context)
 
